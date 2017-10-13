163e40138f35e762bab2e02c3812b4b6d6a1cd4d
coenhallie <coenhallie@gmail.com>
2017-10-13 20:22:37 +0200

    added particles.js

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,80 @@
 <template>
   <section class="container">
-    <div>
-      <vue-particles color="#000000"><h1>test</h1></vue-particles>
-      <logo/>
-      <h1 class="title">
-        project-website
-      </h1>
-      <h2 class="subtitle">
-        my portfolio website
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+    <div class="wrap-banner">
+      <div class="main-title">
+        <h1 class="title">
+          project-website
+        </h1>
+        <h2 class="subtitle">
+          my portfolio website
+        </h2>
+        <div class="links">
+          <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+          <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        </div>
       </div>
+        <vue-particles
+          color="#ddd"
+          :particleOpacity="0.7"
+          :particlesNumber="80"
+          shapeType="circle"
+          :particleSize="5"
+          linesColor="#ddd"
+          :linesWidth="2"
+          :lineLinked="true"
+          :lineOpacity="0.4"
+          :linesDistance="150"
+          :moveSpeed="3"
+          :hoverEffect="true"
+          hoverMode="grab"
+          :clickEffect="true"
+          clickMode="push"
+        >
+        </vue-particles>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
-    Logo
   }
 }
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
+  .container {
+    text-align:center;
+    -ms-flex-align: center;
+    -webkit-box-align: center;
+    align-items: center;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    height:auto;
+  }
+  .main-title {
+    color:#000;
+    z-index:999;
+  }
+  #particles-js {
+    background-size: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+  .wrap-banner {
+  }
+
+  .title {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
   font-weight: 300;
   font-size: 100px;
   color: #35495e;
